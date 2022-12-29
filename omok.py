9359e3c7493100f55531a0cfbd4ec12b0229421b
YoRangS <21900673@handong.ac.kr>
2022-12-30 00:41:09 +0900

    add display

--- omok.py
+++ omok.py
@@ -1,6 +1,14 @@
 def display():
-    pass
-
+    print()
+    print("  ", end=" ")
+    for i in range(1, 16):
+        print("{0:>2}".format(i), end=" ")
+    print()
+    for i in range(1, 16):
+        print("{0:>2}".format(i), end=" ")
+        for j in range(15):
+            print("{}".format(L[i][j]), end=" ")
+        print()
 def is_end():
     pass
 
@@ -8,3 +16,5 @@ def put():
     pass
 
 count = 1
+L = [['  ' for i in range(16)] for _ in range(16)]
+display()
